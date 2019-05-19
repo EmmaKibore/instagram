2df458f137e3b4cb6769d04fa7447c7b579a072c
EmmaKibore <emmakibore@gmail.com>
2019-05-19 03:06:05 +0300

    defined signup function

--- instagram/views.py
+++ instagram/views.py
@@ -1,47 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,
 from django.http import HttpResponse, Http404
 import datetime as dt
-from django.shortcuts import render
+from .email import send_welcome_email
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import ImageForm, SignupForm, CommentForm, EditForm
+from django.db import models
+from .models import Image, Profile
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to instagram ')
 
-def instagram_of_day(request):
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your Instagram account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can <a href="/accounts/login/">Login</a>  your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+def home(request):
     date = dt.date.today()
+    
+    return render(request, 'registration/homepage.html', {"date": date,})
 
-    #Function to convert date object to find exact day
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Instagram for {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)   
-
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day 
-
-def past_days_news(request,past_date):
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-        day = convert_dates(date)
-        html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-        return HttpResponse(html)        
