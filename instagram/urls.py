1c966845311821a2359074428106b2c462e80de7
EmmaKibore <emmakibore@gmail.com>
2019-05-22 15:41:53 +0300

    styled registration templates

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from  .import views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,7 +10,7 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^comment/(?P<image_id>\d+)', views.comment, name='comments'),
     url(r'^profiles/(\d+)',views.profiles,name='profiles'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+    # url(r'^signup/$', views.signup, name='signup'),
+    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     views.activate, name='activate'),
 ]
